292533592139aff3ecaba8efaf213d298618e469
Ethan Harris <ethanwharris@gmail.com>
2022-06-08 20:41:28 +0100

    Add progress bars

--- flashy/ui/src/components/Configurator.tsx
+++ flashy/ui/src/components/Configurator.tsx
@@ -99,22 +99,16 @@ const Demos: Demo[]  = [
 export default function Configurator(props: {lightningState: any, updateLightningState: (newState: any) => void}) {
     const { setSelectedTab } = useSelectedTabState();
 
-    const ready = props.lightningState?.flows.hpo.vars.ready;
-
     const [task, setTask] = React.useState('image_classification');
     const [modelType, setModelType] = React.useState('demo');
     const [targetPerformance, setTargetPerformance] = React.useState('low');
-    const [showLaunchingSpinner, setShowLaunchingSpinner] = React.useState(false);
+    // const [showLaunchingSpinner, setShowLaunchingSpinner] = React.useState(false);
 
     const [dataConfig, setDataConfig] = React.useState(new Map());
     const [demoDataConfig, setDemoDataConfig] = React.useState(Demos[0].config);
 
     function startTraining() {
-        setShowLaunchingSpinner(true);
         if (props.lightningState) {
-            // HACK: Ensures that ready is false
-            props.lightningState.flows.hpo.vars.ready = false
-
             // Create and send new state
             const newLightningState = cloneDeep(props.lightningState);
             newLightningState.flows.hpo.vars.start = true;
@@ -123,6 +117,7 @@ export default function Configurator(props: {lightningState: any, updateLightnin
             newLightningState.flows.hpo.vars.model = modelType;
             newLightningState.flows.hpo.vars.performance = targetPerformance;
 
+            setSelectedTab(1);
             props.updateLightningState(newLightningState);
         }
     }
@@ -136,114 +131,112 @@ export default function Configurator(props: {lightningState: any, updateLightnin
 
     return (
         <>
-            <Box display={!showLaunchingSpinner? "initial": "none"}>
-                <Grid container spacing={3} px={{xs: 6, sm: 12, md: 18}} py={6}>
-                    <Grid item xs={12}>
-                        <Typography variant="h6">Choose your AI task</Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControl>
-                            <RadioGroup
-                                name="task"
-                                row
-                                value={task}
-                                onChange={handleTaskChange}
-                            >
-                                <FormControlLabel value="image_classification" control={<Radio />} label="Image Classification" />
-                                <FormControlLabel value="text_classification" control={<Radio />} label="Text Classification" />
-                            </RadioGroup>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Typography variant="h6">Upload your dataset</Typography>
-                    </Grid>
-                    <DataConfig
-                        dataOptions={loadingOptions[loadingOptions.findIndex((dataOptions: DataOptions) => dataOptions.task == task)]}
-                        value={dataConfig}
-                        example={demoDataConfig}
-                        onChange={setDataConfig}
-                        lightningState={props.lightningState}
-                    />
-                    <Grid item xs={12}>
-                        <Typography variant="h6">Configure your sweep</Typography>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <PillSelect
-                            helperText=""
-                            label="Model type"
-                            onChange={setModelType as (value: any) => void}
-                            options={[
-                                {
-                                    label: "demo",
-                                    value: "Demo",
-                                },
-                                {
-                                    label: "average",
-                                    value: "Average",
-                                },
-                                {
-                                    label: "sota",
-                                    value: "State-of-the-art",
-                                },
-                            ]}
-                            fullWidth
-                            statusText=""
-                            value={modelType}
-                        />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <PillSelect
-                            helperText=""
-                            label="Target performance"
-                            onChange={setTargetPerformance as (value: any) => void}
-                            options={[
-                                {
-                                    label: "low",
-                                    value: "Low",
-                                },
-                                {
-                                    label: "medium",
-                                    value: "Medium",
-                                },
-                                {
-                                    label: "high",
-                                    value: "High",
-                                },
-                            ]}
-                            fullWidth
-                            statusText=""
-                            value={targetPerformance}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Stack direction="row" spacing={2} alignItems="center">
-                            <PillButton text="Start training!" onClick={startTraining}/>
-                            {props.lightningState?.flows.hpo.vars.has_run? [
-                                (<Typography variant="subtitle2">
-                                    <Stack direction="row" alignItems="center">
-                                        <InfoIcon color="primary" fontSize="inherit"/>
-                                        <span style={{lineHeight: 1, marginLeft: "0.25em"}}>Your current run will be deleted</span>
-                                    </Stack>
-                                </Typography>),
-                            ]: []}
-                        </Stack>
-                    </Grid>
+            <Grid container spacing={3} px={{xs: 6, sm: 12, md: 18}} py={6}>
+                <Grid item xs={12}>
+                    <Typography variant="h6">Choose your AI task</Typography>
                 </Grid>
-            </Box>
-            <Box display={showLaunchingSpinner? "initial": "none"} py={12}>
-                <Stack direction="column" alignItems="center">
-                    <CircularProgress thickness={1} size="96px" sx={{my: 3, display: (!ready? "initial": "none")}} />
-                    <SvgIcon sx={{width: "96px", height: "96px", my: 3, display: (ready? "initial": "none")}} viewBox="0 0 96 96">
-                        <GreenTick />
-                    </SvgIcon>
-                    <Typography variant="h5" mb={1}>{ready? "Ready": "Launching..."}</Typography>
-                    <Typography variant="body1" mb={3}>Hang tight!</Typography>
-                    <Stack direction="row" spacing={3}>
-                        <PillButton text="View results" disabled={!ready} onClick={() => setSelectedTab(1)}/>
-                        <PillButton text="Start over" color="grey" onClick={() => setShowLaunchingSpinner(false)}/>
+                <Grid item xs={12}>
+                    <FormControl>
+                        <RadioGroup
+                            name="task"
+                            row
+                            value={task}
+                            onChange={handleTaskChange}
+                        >
+                            <FormControlLabel value="image_classification" control={<Radio />} label="Image Classification" />
+                            <FormControlLabel value="text_classification" control={<Radio />} label="Text Classification" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h6">Upload your dataset</Typography>
+                </Grid>
+                <DataConfig
+                    dataOptions={loadingOptions[loadingOptions.findIndex((dataOptions: DataOptions) => dataOptions.task == task)]}
+                    value={dataConfig}
+                    example={demoDataConfig}
+                    onChange={setDataConfig}
+                    lightningState={props.lightningState}
+                />
+                <Grid item xs={12}>
+                    <Typography variant="h6">Configure your sweep</Typography>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <PillSelect
+                        helperText=""
+                        label="Model type"
+                        onChange={setModelType as (value: any) => void}
+                        options={[
+                            {
+                                label: "demo",
+                                value: "Demo",
+                            },
+                            {
+                                label: "average",
+                                value: "Average",
+                            },
+                            {
+                                label: "sota",
+                                value: "State-of-the-art",
+                            },
+                        ]}
+                        fullWidth
+                        statusText=""
+                        value={modelType}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <PillSelect
+                        helperText=""
+                        label="Target performance"
+                        onChange={setTargetPerformance as (value: any) => void}
+                        options={[
+                            {
+                                label: "low",
+                                value: "Low",
+                            },
+                            {
+                                label: "medium",
+                                value: "Medium",
+                            },
+                            {
+                                label: "high",
+                                value: "High",
+                            },
+                        ]}
+                        fullWidth
+                        statusText=""
+                        value={targetPerformance}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <PillButton text="Start training!" onClick={startTraining}/>
+                        {/*{props.lightningState?.flows.hpo.vars.has_run? [*/}
+                        {/*    (<Typography variant="subtitle2">*/}
+                        {/*        <Stack direction="row" alignItems="center">*/}
+                        {/*            <InfoIcon color="primary" fontSize="inherit"/>*/}
+                        {/*            <span style={{lineHeight: 1, marginLeft: "0.25em"}}>Your current run will be deleted</span>*/}
+                        {/*        </Stack>*/}
+                        {/*    </Typography>),*/}
+                        {/*]: []}*/}
                     </Stack>
-                </Stack>
-            </Box>
+                </Grid>
+            </Grid>
+            {/*<Box display={showLaunchingSpinner? "initial": "none"} py={12}>*/}
+            {/*    <Stack direction="column" alignItems="center">*/}
+            {/*        <CircularProgress thickness={1} size="96px" sx={{my: 3, display: (!ready? "initial": "none")}} />*/}
+            {/*        <SvgIcon sx={{width: "96px", height: "96px", my: 3, display: (ready? "initial": "none")}} viewBox="0 0 96 96">*/}
+            {/*            <GreenTick />*/}
+            {/*        </SvgIcon>*/}
+            {/*        <Typography variant="h5" mb={1}>{ready? "Ready": "Launching..."}</Typography>*/}
+            {/*        <Typography variant="body1" mb={3}>Hang tight!</Typography>*/}
+            {/*        <Stack direction="row" spacing={3}>*/}
+            {/*            <PillButton text="View results" disabled={!ready} onClick={() => setSelectedTab(1)}/>*/}
+            {/*            <PillButton text="Start over" color="grey" onClick={() => setShowLaunchingSpinner(false)}/>*/}
+            {/*        </Stack>*/}
+            {/*    </Stack>*/}
+            {/*</Box>*/}
         </>
     )
 }
