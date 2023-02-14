fae7c61d6f7874ab6d0c473c97fc25c5bc8eb2ff
Jirka <jirka.borovec@seznam.cz>
2023-02-14 16:51:29 +0100

    - isort

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import os
-import sys
-
-sys.path.append(os.path.dirname(__file__))
 
 import lightning as L
 from lightning.app.frontend import StaticWebFrontend  # noqa: E402
@@ -53,4 +50,5 @@ class Flashy(L.LightningFlow):
         ]
 
 
-app = L.LightningApp(Flashy())
+if __name__ == "__main__":
+    app = L.LightningApp(Flashy())
